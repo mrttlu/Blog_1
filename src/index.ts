aa9537d6b5786a9688dc4f35a95a66eddc1e89c3
mrttlu <myyk@mrt.ee>
2021-10-02 14:29:41 +0300

    Add blog posts endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,77 @@
 import express, { Request, Response, Application } from 'express';
+import { nanoid } from 'nanoid';
 
 const app: Application = express();
 
 const port: number = 3000;
 
-app.get('/ping', (req: Request, res: Response) => {
-  res.status(200).json({
-    message: 'Alive',
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+  author: string;
+}
+
+interface Db {
+  posts: Post[];
+}
+
+const db: Db = {
+  posts: [
+    {
+      id: 'qwert',
+      title: 'Esimene postitus',
+      content: 'Tähtis postitus',
+      author: 'Mrt',
+    },
+    {
+      id: 'asdf',
+      title: 'Teine postitus',
+      content: 'Vähemtähtis postitus',
+      author: 'Mrt',
+    },
+  ],
+};
+
+app.use(express.json());
+
+app.get('/ping', (req: Request, res: Response) => res.status(200).json({
+  message: 'Alive',
+}));
+
+app.get('/posts', (req: Request, res: Response) => {
+  const { posts } = db;
+
+  return res.status(200).json({
+    posts,
+  });
+});
+
+app.get('/posts/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const post = db.posts.find((element: Post) => element.id === id);
+  if (!post) {
+    return res.status(400).json({
+      messsage: `No post exists with id: ${id}`,
+    });
+  }
+  return res.status(200).json({
+    post,
+  });
+});
+
+app.post('/posts', (req: Request, res: Response) => {
+  const { title, content, author } = req.body;
+  const id = nanoid();
+  const post: Post = {
+    id,
+    title,
+    content,
+    author,
+  };
+  db.posts.push(post);
+  return res.status(200).json({
+    id: post.id,
   });
 });
 
